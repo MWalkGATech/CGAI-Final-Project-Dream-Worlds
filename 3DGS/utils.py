95fba16228b777b4436ba1444b2d050b3fce678b
Michael Walker <killyosaur@gmail.com>
2026-04-26 14:44:23 -0400

    inverse sigma for 3D gaussian (uses quaternions)

--- 3DGS/utils.py
+++ 3DGS/utils.py
@@ -42,3 +42,31 @@ def loss_results(prediction, target, lam=.2, window_size=11, size_average=True, 
     d_ssim = (1 - SSIM(prediction, target, window_size=window_size, size_average=size_average, device=device)).squeeze(0)
     
     return nn.functional.l1_loss(prediction, target) * (1 - lam) + d_ssim.item() * lam
+
+
+def build_sigma_inv(quat: torch.Tensor, sigma: torch.Tensor):
+    w_2 = quat[0] * quat[0]
+    x_2 = quat[1] * quat[1]
+    y_2 = quat[2] * quat[2]
+    z_2 = quat[3] * quat[3]
+
+    xy = quat[1] * quat[2]
+    wz = quat[0] * quat[3]
+    xz = quat[1] * quat[3]
+    wy = quat[0] * quat[2]
+    wx = quat[0] * quat[1]
+    yz = quat[2] * quat[3]
+
+    R = torch.Tensor([
+        [2 * (w_2 + x_2) - 1, 2 * (xy - wz), 2 * (xz + wy)],
+        [2 * (xy + wz), 2 * (w_2 + y_2) - 1, 2 * (xz - wx)],
+        [2 * (xz - wy), 2 * (yz + wx), 2 * (w_2 + z_2) - 1],
+    ], device=quat.device)
+
+    S = torch.eye(3, device=sigma.device)
+    
+    sigma_square_safe = (sigma * sigma) + 1e-8
+
+    S *= 1 / sigma_square_safe
+
+    return R @ S @ R.T
